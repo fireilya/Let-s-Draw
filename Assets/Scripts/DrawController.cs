dec7d55d3c36af9689d735e8a55f7ff7c7469481
fireilya <117916135+fireilya@users.noreply.github.com>
2024-03-16 21:51:45 +0500

    Initial commit

---
+++ Assets/Scripts/DrawController.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawController : MonoBehaviour
+{
+    [SerializeField]
+    private LineRenderer linePrefab;
+
+    [SerializeField]
+    private Color lineColor;
+
+    [SerializeField]
+    [Range(1e-5f, 1)]
+    private float lineWidth;
+
+    [SerializeField]
+    [Range(0f, float.MaxValue)]
+    private float drawingTimeLimit;
+
+    [SerializeField]
+    private bool isLineDissapear;
+
+    [SerializeField]
+    private bool isLineWithColider;
+
+    [SerializeField]
+    private Sprite lineTexture;
+
+    private LineRenderer currentLine;
+    private float currentLineZIndex = 0;
+    private float currentDrawingTime;
+    private EdgeCollider2D currentLineCollider;
+    private List<Vector2> currentLinePositions = new List<Vector2>();
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            InitLine();
+        }
+
+        if(Input.GetMouseButton(0) && currentDrawingTime<=drawingTimeLimit)
+        {
+            Draw();
+        }
+
+        if (Input.GetMouseButtonUp(0) && isLineDissapear)
+        {
+            Destroy(currentLine);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            clearAllLines();
+        }
+
+    }
+
+    private void clearAllLines()
+    {
+        foreach(var line in FindObjectsOfType<LineRenderer>())
+        {
+            Destroy(line.gameObject);
+        }
+    }
+
+    private void Draw() 
+    {
+        currentLine.positionCount++;
+        var cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currentLine.SetPosition(currentLine.positionCount - 1, new Vector3(cursorPosition.x, cursorPosition.y, currentLineZIndex));
+        currentLinePositions.Add(new Vector2(cursorPosition.x, cursorPosition.y));
+        currentDrawingTime += Time.deltaTime;
+        if (isLineWithColider)
+        {
+            currentLineCollider.SetPoints(currentLinePositions);
+        }
+    }
+
+    private void InitLine()
+    {
+        currentLine = Instantiate(linePrefab, Vector3.zero, Quaternion.identity);
+        currentLine.widthCurve = new AnimationCurve(new Keyframe(0, lineWidth), new Keyframe(1, lineWidth));
+        Material lineMaterial = currentLine.material;
+
+        if (lineTexture == null)
+            lineMaterial.color = lineColor;
+        else
+            lineMaterial.mainTexture = lineTexture.texture;
+
+        currentLine.textureScale = new Vector2(1f / lineWidth, -1);
+        currentLineZIndex -= 1e-3f;
+        currentDrawingTime = 0;
+        if (isLineWithColider)
+        {
+            currentLineCollider = currentLine.GetComponent<EdgeCollider2D>();
+            currentLineCollider.enabled = true;
+        }
+        currentLinePositions.Clear();
+    }
+}
