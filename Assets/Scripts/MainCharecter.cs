4530909d5e0c4753266644eb3235ddd588a1f2bb
fireilya <117916135+fireilya@users.noreply.github.com>
2024-04-16 17:37:33 +0500

    Go to finish mechanic finished

---
+++ Assets/Scripts/MainCharecter.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharecter : MonoBehaviour
+{
+    [SerializeField]
+    private float pushForce;
+
+    [SerializeField]
+    private float maxVelocity;
+
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Static;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="Finish")      
+            Debug.Log("VICTORY");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            var previousVelocity = rb.velocity;
+            if (rb.bodyType == RigidbodyType2D.Static)
+                rb.bodyType = RigidbodyType2D.Dynamic;
+            rb.AddForce(Vector2.right * pushForce);
+            rb.velocity = rb.velocity.magnitude < maxVelocity ? rb.velocity : previousVelocity;
+        }
+    }
+}
