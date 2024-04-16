d4887a785004171bc74e7b086c38a0c42bf0386d
fireilya <117916135+fireilya@users.noreply.github.com>
2024-04-16 17:09:05 +0500

    DrawPart Mechanic Completed

--- Assets/Scripts/PartToDraw.cs
+++ Assets/Scripts/PartToDraw.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PartToDraw : MonoBehaviour
 {
     [SerializeField]
     private DrawController drawController;
+
+    [SerializeField]
+    private float accuracyCoeff;
 
     private PolygonCollider2D poligonCollider;
     private SpriteRenderer spriteRenderer;
@@ -14,10 +18,19 @@ public class PartToDraw : MonoBehaviour
     {
         poligonCollider = GetComponent<PolygonCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        drawController.OnLineFinished.AddListener(VerifyStroke);
     }
 
-    void VerifyStroke(List<Vector2> linePointPositiions)
+    void VerifyStroke(List<Vector2> linePointPositions)
     {
+        var deviation = poligonCollider.points
+            .Select(coliderOutlinePoint => 
+            linePointPositions
+                .Select(linePoint => Vector2.Distance(transform.TransformPoint(coliderOutlinePoint), linePoint))
+                .Min())
+            .Average();
 
+        if (deviation<accuracyCoeff)       
+            spriteRenderer.enabled = true;
     }
 }
