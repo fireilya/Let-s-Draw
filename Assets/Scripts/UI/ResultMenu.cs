b9d7a4daee07383c266e3756680caa16af78f0cf
fireilya <117916135+fireilya@users.noreply.github.com>
2024-05-28 14:52:15 +0500

    Result menu Ready

--- Assets/Scripts/UI/ResultMenu.cs
+++ Assets/Scripts/UI/ResultMenu.cs
@@ -1,11 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour
+public class ResultMenu : MonoBehaviour
 {
     public void GoToMainMenu() { SceneManager.LoadScene(0); }
-    public void Resume() { Time.timeScale = 1f; gameObject.SetActive(false); }
+    public void Restart() { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); }
 }
